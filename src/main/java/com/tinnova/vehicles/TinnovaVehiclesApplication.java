69c63a0c118a88a36161b97c73275e235d1f5b7f
Guilherme <guilherme199728@gmail.com>
2023-02-10 21:39:38 -0900

    criação projeto

---
+++ src/main/java/com/tinnova/vehicles/TinnovaVehiclesApplication.java
@@ -0,0 +1,13 @@
+package com.tinnova.vehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinnovaVehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinnovaVehiclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinnova/vehicles/TinnovaVehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinnova.vehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinnovaVehiclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
