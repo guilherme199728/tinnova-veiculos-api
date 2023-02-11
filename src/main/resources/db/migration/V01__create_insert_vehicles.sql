b8f66ccddd5173dbb21bbf73834325b906f45bc6
Guilherme <guilherme199728@gmail.com>
2023-02-10 22:09:37 -0900

    criando migration

---
+++ src/main/resources/db/migration/V01__create_insert_vehicles.sql
@@ -0,0 +1,46 @@
+CREATE TABLE vehicles (
+    id SERIAL PRIMARY KEY,
+    brand VARCHAR(50) NOT NULL,
+    year INTEGER NOT NULL,
+    description VARCHAR(256) NOT NULL,
+    sold BOOLEAN NOT NULL,
+    creation_date TIMESTAMP NOT NULL,
+    modification_date TIMESTAMP NOT NULL
+);
+
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Chevrolet', 2016, 'Cruze', false, '04/04/2021', '04/04/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Ford', 2003, 'Ford ka', true, '04/04/2021', '04/04/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Volkswagen', 1980, 'Fusca', false, '04/03/2021', '04/03/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Fiat', 2015, 'Argo', false, '04/03/2021', '04/03/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Ferrari', 2020, 'Portofino', true, '04/03/2021', '04/03/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Porsche', 2020, 'Cayenne', true, '04/03/2021', '04/03/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Chevrolet', 2016, 'Cruze', false, '04/04/2021', '04/04/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Ford', 2003, 'Ford ka', true, '04/04/2021', '04/04/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Volkswagen', 1980, 'Fusca', false, '04/03/2021', '04/03/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Fiat', 2015, 'Argo', false, '04/03/2021', '04/03/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Ferrari', 2020, 'Portofino', false, '04/03/2021', '04/03/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Porsche', 2020, 'Cayenne', true, '04/03/2021', '04/03/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Chevrolet', 2016, 'Cruze', true, '04/04/2021', '04/04/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Ford', 2003, 'Ford ka', true, '04/04/2021', '04/04/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Volkswagen', 1980, 'Fusca', false, '04/03/2021', '04/03/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Fiat', 2015, 'Argo', false, '04/03/2021', '04/03/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Ferrari', 2020, 'Portofino', true, '04/03/2021', '04/03/2021');
+
+insert into vehicles (brand, year, description, sold, creation_date, modification_date) values ('Porsche', 2020, 'Cayenne', false, '04/03/2021', '04/03/2021');
